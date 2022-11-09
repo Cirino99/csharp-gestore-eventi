82c4e2285aa007ccc3211c72247d9635414102b4
Simone <cirino.simone99@gmail.com>
2022-11-09 14:27:20 +0100

    creata claase evento ed eccezione

---
+++ Exception/GestoreEventiException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi.Exception
+{
+    public class GestoreEventiException : Exception
+    {
+        public GestoreEventiException()
+        {
+
+        }
+        public GestoreEventiException(string? message ) : base(message)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using csharp_gestore_eventi.Exception;
+
 Console.WriteLine("Hello, World!");
+
+public class Evento
+{
+    private string _titolo;
+    public string Titolo { 
+        get
+        {
+            return _titolo;
+        }
+        set
+        {
+            if (value == null || value == "")
+                throw new GestoreEventiException("Il nome passato è vuoto");
+            _titolo = value;
+        }
+    }
+    private DateOnly _data;
+    public DateOnly Data {
+        get
+        {
+            return _data;
+        }
+        set
+        {
+            _data = value;
+        }
+    }
+    private int _capienzaMassima;
+    public int CapienzaMassima {
+        get
+        {
+            return _capienzaMassima;
+        }
+        private set
+        {
+            _capienzaMassima = value;
+        }
+    }
+    public int PostiPrenotati { get; private set; }
+    public Evento(string titolo, string data, int capienzaMassima)
+    {
+
+    }
+}
