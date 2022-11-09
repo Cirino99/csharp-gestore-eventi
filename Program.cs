332d4c884c012ac255ff7caae89d450b3278d399
Simone <cirino.simone99@gmail.com>
2022-11-09 15:11:37 +0100

    creato main di test eccezioni

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Creazione evento");
+Console.WriteLine("Inserisci il nome");
+string nome = Console.ReadLine();
+Console.WriteLine("Inserisci la data in formato  gg/mm/yyyy");
+string dataStringa = Console.ReadLine();
+DateOnly data = DateOnly.Parse(dataStringa);
+Console.WriteLine("Inserisci la capienza massima");
+int capienza = Convert.ToInt32(Console.ReadLine());
+try
+{
+    Evento evento = new Evento(nome, data, capienza);
+    Console.WriteLine(evento.ToString());
+} catch(GestoreEventiException e)
+{
+    Console.WriteLine(e.ToString());
+}
